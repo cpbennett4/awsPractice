1f25b018d12bf5cab9c3903ef0b10ea1e189b621
Charles Paul Bennett <cpbennett4@gmail.com>
2018-07-31 13:44:35 -0700

    Setup pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'awsPractice',
+    script: 'server/app.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-193-52-171.us-west-1.compute.amazonaws.com',
+      key: '~/.ssh/awsPractice',
+      ref: 'origin/master',
+      repo: 'git@github.com:cpbennett4/awsPractice.git',
+      path: '/home/ubuntu/awsPractice',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+};
