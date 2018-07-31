00f1227d374a802e3dc102fc72675138675d9896
Charles Paul Bennett <cpbennett4@gmail.com>
2018-07-31 14:49:18 -0700

    Use newNodeSandbox.pem as key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-54-193-52-171.us-west-1.compute.amazonaws.com',
-      key: '~/.ssh/id_rsa',
+      key: '~/.ssh/newNodeSandbox.pem',
       ref: 'origin/master',
       repo: 'git@github.com:cpbennett4/awsPractice.git',
       path: '/home/ubuntu/awsPractice',
