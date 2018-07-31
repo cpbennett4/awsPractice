03cef4f92d065736edda727f3cb7bd926ec9fff5
Charles Paul Bennett <cpbennett4@gmail.com>
2018-07-31 13:59:13 -0700

    Modify config file for correct key

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-54-193-52-171.us-west-1.compute.amazonaws.com',
-      key: '~/.ssh/awsPractice',
+      key: '~/.ssh/id_rsa',
       ref: 'origin/master',
       repo: 'git@github.com:cpbennett4/awsPractice.git',
       path: '/home/ubuntu/awsPractice',
